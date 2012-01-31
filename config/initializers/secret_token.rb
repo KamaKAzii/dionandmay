8b6d09ab753cf58d6ae4f4d21933ee3d78d73086
KamaKAzii <spskamakazii@gmail.com>
2012-01-31 20:24:56 +1100

    Cleanup and add welcome controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dionandmay::Application.config.secret_token = 'a8860d8fe3f2a7cfd7ae11f6d643f723f6b959c1d472124b23ffa9069e126756412add1ff9cf7d6ecab5e3f1e5fc0ac9778bcd95188e579853214c1af5714a67'
