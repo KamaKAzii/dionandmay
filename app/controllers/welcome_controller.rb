343675af0dc4dabc4a6b7329b009e5ccdf51c2ca
KamaKAzii <spskamakazii@gmail.com>
2012-02-29 21:55:57 +1100

    Added API controller which will contain all AJAX controller methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,17 +2,6 @@ class WelcomeController < ApplicationController
 
   def index
 
-    def resource_name
-      :user
-    end
-
-    def resource
-      @resource ||= User.new
-    end
-
-    def devise_mapping
-      @devise_mapping ||= Devise.mappings[:user]
-    end
 
   end
 
